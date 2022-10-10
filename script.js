18add250eb22c362da0266aa52fdff6f40e8c98b
MaltaWebDev <MaltaWebDev@gmail.com>
2022-10-10 21:37:54 +0200

    Add playerScore

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 // create random number between 1 and 20, assign to variable secretNumber
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
+let playerScore = 20;
+
 document.querySelector(".number").textContent = secretNumber;
 
 // add event listener to the check button
@@ -20,8 +22,12 @@ document.querySelector(".check").addEventListener("click", function () {
         "🚫 Number greater than 20!";
     } else {
       document.querySelector(".message").textContent = "📈 Too high!";
+      playerScore--;
+      document.querySelector(".score").textContent = playerScore;
     }
   } else if (guess < secretNumber) {
     document.querySelector(".message").textContent = "📉 Too low!";
+    playerScore--;
+    document.querySelector(".score").textContent = playerScore;
   }
 });
