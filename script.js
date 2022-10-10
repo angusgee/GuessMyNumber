5f4b5094f4b9d694df41b1971e76f9a2fdf9fc92
MaltaWebDev <MaltaWebDev@gmail.com>
2022-10-10 19:16:18 +0200

    Update link to stylesheet, add code to script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 "use strict";
+
+console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".score").textContent = 10;
+
+document.querySelector(".number").textContent = 13;
