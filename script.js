a49b6ce6c07695a2010c88bfafdebd1240baaea2
MaltaWebDev <MaltaWebDev@gmail.com>
2022-10-10 19:28:55 +0200

    Add query selectors to script.js

--- script.js
+++ script.js
@@ -2,6 +2,13 @@
 
 console.log(document.querySelector(".message").textContent);
 
+document.querySelector(".message").textContent = "🎉 Correct Number!";
+
+console.log(document.querySelector(".message").textContent);
+
 document.querySelector(".score").textContent = 10;
 
 document.querySelector(".number").textContent = 13;
+
+console.log(document.querySelector(".guess").value);
+document.querySelector(".guess").value = 23;
